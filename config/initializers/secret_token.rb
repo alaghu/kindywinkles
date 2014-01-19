69840813bf1ac2a51f877e267513bcb9d1c8856e
Anand <anand@loremore.com>
2014-01-18 20:22:33 -0800

    initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+KindywinklesCom::Application.config.secret_key_base = 'ef4ff0c21276859eb517d3d7231a3d1a66b150356c3b3c2153c4418fe00fc944be3650ecf299d39eb037d2e5b2ff72075140f1065c0f9cc704d9908fad3acea9'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+KindywinklesCom::Application.config.session_store :cookie_store, key: '_kindywinkles_com_session'
