50a0d9ed34fa20a1c90564ea22f6e7010500228d
Anand <anand@loremore.com>
2014-01-27 21:41:38 -0800

    more text on the pages, and their corresponding texts.Only styling remaining.

--- spec/features/home_page_spec.rb
+++ spec/features/home_page_spec.rb
@@ -17,9 +17,26 @@ describe 'Home Page' do
     page.should have_content "Care"
   end
 
-  it "should have the title 'Kindy Winkles. Art * Music * Care'" do
+  it "should have the title 'Kindy Winkles. Art • Music • Care'" do
     visit "/"
     page.should have_title "Kindy Winkles. Art • Music • Care"
   end
+
+  it "should have address 748 danforth ter sunnyvale 94087" do
+    visit "/"
+    page.should have_text "748 danforth ter sunnyvale 94087"
+  end
+
+
+  it "should have email priya@kindywinkles.com" do
+    visit "/"
+    page.should have_link "priya@kindywinkles.com"
+  end
+
+  it "should have phone 650 888 4845" do
+    visit "/"
+    page.should have_text "650-888-4845"
+  end
+
 
 end
