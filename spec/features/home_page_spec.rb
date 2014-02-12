827ddbfadebe3104172f1a0a2b07d71dc8aed988
Anand <anand@loremore.com>
2014-02-11 18:55:21 -0800

    refactored the test by consolidating the text based checks using a each iterator.'

--- spec/features/home_page_spec.rb
+++ spec/features/home_page_spec.rb
@@ -2,42 +2,25 @@ require 'spec_helper'
 
 describe 'Home Page' do
 
-  it "Should have text 'Art'" do
+  it "Should have text in the array" do
     visit "/"
-    page.should have_content "Art"
+    content = ["Art", "Music", "Care", "748 danforth ter sunnyvale 94087",
+               "650-888-4845"]
+
+    content.each do |content_element|
+      page.should have_text content_element
+    end
+
   end
 
-  it "should have text 'Music'" do
-    visit "/"
-    page.should have_content "Music"
-  end
-
-  it "should have text 'Care'" do
-    visit "/"
-    page.should have_content "Care"
-  end
-
-  it "should have the title 'Kindy Winkles. Art • Music • Care'" do
+  it "should have title 'Kindy Winkles. Art • Music • Care'" do
     visit "/"
     page.should have_title "Kindy Winkles. Art • Music • Care"
   end
 
-  it "should have address 748 danforth ter sunnyvale 94087" do
-    visit "/"
-    page.should have_text "748 danforth ter sunnyvale 94087"
-  end
-
-
-  it "should have email priya@kindywinkles.com" do
+  it "should have email link priya@kindywinkles.com" do
     visit "/"
     page.should have_link "priya@kindywinkles.com"
   end
-
-  it "should have phone 650 888 4845" do
-    visit "/"
-    page.should have_text "650-888-4845"
-
-  end
-
 
 end
